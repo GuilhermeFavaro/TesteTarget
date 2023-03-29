9e7394a5bbc2675fe5fa4953f8395c034d251275
Guilherme Fávaro <70180021+GuilhermeFavaro@users.noreply.github.com>
2023-03-29 16:17:56 -0300

    Add files via upload

---
+++ Teste Porcentagem.py
@@ -0,0 +1,18 @@
+SP = 67.83643
+RJ = 36.67866
+MG = 29.22988
+ES = 27.16548
+OT = 19.84953
+TOTAL = (SP + RJ + MG + ES + OT)
+
+prcSP = ((SP/TOTAL)*100)
+prcRJ = ((RJ/TOTAL)*100)
+prcMG = ((MG/TOTAL)*100)
+prcES = ((ES/TOTAL)*100)
+prcOT = ((OT/TOTAL)*100)
+
+print('Porcentagem SP {}'.format(prcSP))
+print('Porcentagem RJ {}'.format(prcRJ))
+print('Porcentagem MG {}'.format(prcMG))
+print('Porcentagem ES {}'.format(prcES))
+print('Porcentagem OT {}'.format(prcOT))
